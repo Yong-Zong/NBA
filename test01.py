8c0fc5bac2d171b8a56eb3ed7d22f373c44863c8
Yong Zong <0808yongzong@gmail.com>
2022-12-01 15:15:57 +0800

    update

--- test01.py
+++ test01.py
@@ -15,7 +15,7 @@ X, y = data[['AW','AL','BW','BL','A10','B10']], data[['WL']]
 print(X.shape, y.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(
-     X, y, test_size=0.2, random_state=70)
+     X, y, test_size=0.2, random_state=0)
 
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
@@ -29,8 +29,8 @@ score = accuracy_score(y_test, ypred)
 print(score)
 # adiff=y_test.iloc[:, 0]-ypred[:, 0]
 # bdiff=y_test.iloc[:, 1]-ypred[:, 1]
-# # plt.plot(np.arange(len(y_test)), y_test.iloc[:, 0],'o-')
-# # plt.plot(np.arange(len(y_test)), ypred[:, 0],'o-')
+# plt.plot(np.arange(len(y_test)), y_test.iloc[:, 0],'o-')
+# plt.plot(np.arange(len(y_test)), ypred[:, 0],'o-')
 # plt.plot(np.arange(len(y_test)),adiff,'o-')
 # plt.plot(np.arange(len(y_test)),bdiff,'o-')
 # plt.legend(['AD','BD'])
